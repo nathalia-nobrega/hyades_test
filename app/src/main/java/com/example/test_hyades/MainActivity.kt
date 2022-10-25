bad67adaabd8df92f1dfced65ed720783bf070c4
nathalia-nobrega <ttnast05@gmail.com>
2022-10-25 09:54:37 -0400

    Second commit

--- app/src/main/java/com/example/test_hyades/MainActivity.kt
+++ app/src/main/java/com/example/test_hyades/MainActivity.kt
@@ -37,12 +37,10 @@ class MainActivity : AppCompatActivity() {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "signInWithEmail:success")
                     val user = auth.currentUser
                     updateUI(user)
                 } else {
-                    // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInWithEmail:failure", task.exception)
                     updateUI(null)
                 }
